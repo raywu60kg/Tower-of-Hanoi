3dc41367c09bfd51b5b006bcb1f8e0a962409586
raywu <wu.haoshiang@gmail.com>
2019-08-27 00:06:49 +0800

    finish

--- Tower-of-Hanoi2/app.R
+++ Tower-of-Hanoi2/app.R
@@ -11,24 +11,7 @@ library(shiny)
 # source("/home/wu/projects/Tower-of-Hanoi/Tower-of-Hanoi/utils.R")
 source("utils.R")
 ui <- fluidPage(
-    
-    # Application title
-    # titlePanel("Tower of Hanoi"),
-    # Sidebar with a slider input for number of bins
-    # sidebarLayout(
-    #     sidebarPanel(
-    #         sliderInput("bins",
-    #                     "Number of bins:",
-    #                     min = 1,
-    #                     max = 50,
-    #                     value = 30)
-    #     ),
-    # 
-    #     # Show a plot of the generated distribution
-    #     mainPanel(
-    #         plotOutput("distPlot")
-    #     )
-    # ),
+    titlePanel("Tower of Hanoi"),
     actionButton("hold", "Hold"),
     actionButton("move_left", "Move left"),
     actionButton("move_right", "Move right"),
@@ -36,25 +19,36 @@ ui <- fluidPage(
 )
 
 
+# server <- function(input, output){
+#     v <- reactiveValues(data = tower_status)
+#     observeEvent(input$hold, {
+#         v$data = hold(v$data)
+#     })
+#     observeEvent(input$move_left, {
+#         v$data = move_left(v$data)
+#     })
+#     observeEvent(input$move_right, {
+#         v$data = move_right(v$data)
+#     })
+#     output$plot <- renderPlot({
+#         plot_tower_status(v$data)
+#     })
+# }
+
 server <- function(input, output){
     v <- reactiveValues(data = tower_status)
     observeEvent(input$hold, {
-        v$data = hold(v$data)
-        # print(v$data)
+        v$data = hold2(v$data)
     })
     observeEvent(input$move_left, {
-        v$data = move_left(v$data)
+        v$data = move(v$data,-1)
     })
     observeEvent(input$move_right, {
-        v$data = move_right(v$data)
+        v$data = move(v$data,+1)
     })
-    
     output$plot <- renderPlot({
-        # print("=======")
-        # print(v$data)
-        # if (is.null(v$data)) return()
         plot_tower_status(v$data)
     })
-    
 }
+    
 shinyApp(ui, server)
--- Tower-of-Hanoi2/utils.R
+++ Tower-of-Hanoi2/utils.R
@@ -8,7 +8,9 @@ tower_status = list(
     small_ring = 1,
     medium_ring = 2,
     big_ring = 3
-  )
+  ),
+  bar_order = c("first_bar", "second_bar", "third_bar"),
+  move_step = 0
 )
 
 hold <- function(tower_status){
@@ -35,6 +37,68 @@ hold <- function(tower_status){
     )}
   return(tower_status)
 }
+
+hold2 <- function(tower_status){
+  if(is.null(tower_status$hold_ring) == FALSE){
+    tower_status$hold_ring = c()
+  }else{
+    bar_index = tower_status$move_step %% 3 + 1
+    target_bar_status = tower_status[[paste(tower_status$bar_order[bar_index],"_status",sep="")]]
+    if(is.null(target_bar_status) == TRUE){
+      return(tower_status)
+    }else{
+      tower_status$hold_ring = target_bar_status[1]
+    }
+  }
+  return(tower_status)
+}
+
+move <- function(tower_status, move_direction){
+  if(is.null(tower_status$hold_ring) == TRUE){
+    
+    # move without hold
+    tower_status$move_step = tower_status$move_step + move_direction
+    bar_index = tower_status$move_step %% 3 + 1
+    tower_status$point_position = tower_status$bar_order[bar_index]
+    return(tower_status)
+  }else{
+    
+    # move with hold
+    current_ring_weight = tower_status$ring_weight[[tower_status$hold_ring]]
+    current_bar_index = tower_status$move_step %% 3 + 1
+    current_bar_status = tower_status[[paste(tower_status$bar_order[current_bar_index],"_status",sep="")]]
+    target_bar_index = (tower_status$move_step + move_direction)  %% 3 + 1
+    target_bar_status = tower_status[[paste(tower_status$bar_order[target_bar_index],"_status",sep="")]]
+    
+    if(is.null(target_bar_status) == TRUE){
+      target_ring_weight = 4
+    }else{
+      target_ring_weight = tower_status$ring_weight[[target_bar_status[1]]]
+    }
+    
+    # decide can move or not
+    if(current_ring_weight > target_ring_weight){
+      return(tower_status) 
+    }else{
+      # move point_position
+      tower_status$move_step = tower_status$move_step + move_direction
+      bar_index = tower_status$move_step %% 3 + 1
+      tower_status$point_position = tower_status$bar_order[bar_index]
+      
+      # change bar status
+      if(length(current_bar_status) == 1){
+        tower_status[[paste(tower_status$bar_order[current_bar_index],"_status",sep="")]] = c()
+      }else{
+        tower_status[[paste(tower_status$bar_order[current_bar_index],"_status",sep="")]] = current_bar_status[-1]
+      }
+      tower_status[[paste(tower_status$bar_order[target_bar_index],"_status",sep="")]] = c(tower_status$hold_ring, target_bar_status)
+      
+    }
+  }
+  return(tower_status)
+}
+      
+    
 
 
 move_left <- function(tower_status){
